3e714522b8c70853d9fe7ef904c8eb98c0710f47
seunghyun <jsh3990@gmail.com>
2025-08-25 17:12:09 +0900

    js

---
+++ 01기초문법/ex03.js
@@ -0,0 +1,16 @@
+
+//대소문자를 전부 구분함
+var name = "홍길동 ";
+var Name = "이순신"; 
+
+console.log(name, Name); 
+
+//붙어있는 단어의 첫글자는 대문자로 사용한다.
+//이름에 띄어쓰기는 들어갈 수 없다.
+var phoneNumber = 1234;
+
+//키워드는 이름으로 쓸 수 없음
+/* var class = 1;
+var for = 1;
+var if = 1;
+var var = 1; */
