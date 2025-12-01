adbac7b8904620598258c1fafbf142edc4671552
seunghyun <jsh3990@gmail.com>
2025-12-01 16:53:55 +0900

    js es06

---
+++ 17JS_ES06/script07.js
@@ -0,0 +1,12 @@
+
+//모듈 익스포트
+//모듈 - 미리 작성해놓은 JS 파일
+//모듈 안에는 클래스, 함수, 변수, 인터페이스 등이 정의되어있음
+
+
+//모듈을 내보낼때는 export 키워드로 내보낸다
+export const name = "홍길동";
+export const age = 20;
+export const getInfo = () => {
+
+}
